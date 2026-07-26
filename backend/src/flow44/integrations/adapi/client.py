e77079d1bc27b0bfe0c376d2d966b0a09c393de9
Oran <orangilboa@gmail.com>
2026-07-26 13:42:02 +0300

    fix lint error

--- backend/src/flow44/integrations/adapi/client.py
+++ backend/src/flow44/integrations/adapi/client.py
@@ -60,7 +60,7 @@ class AdapiClient:
             return set()
         return {dn for dn in match.member_of if dn}
 
-    async def search_users(self, accountOrEmailOrName: str) -> list[AdUser]:
+    async def search_users(self, account_or_email_or_name: str) -> list[AdUser]:
         return await self._search_users(self._fuzzy_params(accountOrEmailOrName))
 
     async def _search_users(self, params: dict[str, str]) -> list[AdUser]:
