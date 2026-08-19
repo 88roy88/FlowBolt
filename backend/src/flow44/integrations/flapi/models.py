d7903cfd5edeb16253b832b0eb2074af9b55966e
yoni5353 <yoni.sku@gmail.com>
2026-08-19 12:48:58 +0300

    Allow empty package description (#198)

--- backend/src/flow44/integrations/flapi/models.py
+++ backend/src/flow44/integrations/flapi/models.py
@@ -195,6 +195,6 @@ class Query(PascalCaseBaseModel):
 class PackageMetadata(PascalCaseBaseModel):
     id_: int = Field(alias="Id")
     name: str
-    description: str
+    description: str | None = None
     # output_queries_id: list[Any]
     queries: list[Query]
